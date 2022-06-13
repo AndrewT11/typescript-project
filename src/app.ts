5b17b78fa8484126059f33b6c731d85ba7da8ee8
Andrew Tran <tranandrew@gmail.com>
2022-06-13 14:53:44 -0500

    npm install and basic application groundwork

---
+++ src/app.ts
@@ -0,0 +1,2 @@
+// With this project, I need to render HTML from template, then take information input from input fields, validates the information. listens to click of submit button, then creates a new project (JS object stored in some array) where the array is rendered to the list (li and ul templates) and the entire list is added to the DOM (id="root"). 
+// The reason we have an <template> with li and a <template> with ul is that we are creating a drag and drop list. So the li elements are going to be the drag elements, and we will be dropping into the <ul> list.
