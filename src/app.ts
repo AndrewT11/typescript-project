27705cfdd277c23df0fe36701871aecf5616ea96
Andrew Tran <tranandrew@gmail.com>
2022-06-28 15:18:46 -0500

    add class Component with generics

--- src/app.ts
+++ src/app.ts
@@ -64,8 +64,9 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
         }
     }
     return adjDescriptor;
-
 }
+
+
 
 // Validation
 // remember, interface is just to set a template of what a validatable type is.The validator function will need to have the same properties as it is typed to Validatable. Value will always be required. The other keys are optional, thus have (?).
@@ -99,6 +100,32 @@ function validate(validatableInput: Validatable) {
   }
 
   return isValid;
+}
+// Component Base Class
+class Component <T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string | undefined,
+  ) {
+    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+    const importedNode = document.importNode(this.templateElement.content, true); // pass a pointer at template element content. second argument is should this be a deep clone or not. If true, all levels of nesting inside of the template will come along. 
+
+    this.element = importedNode.firstElementChild as U; 
+    if(newElementId) {
+      this.element.id = newElementId;
+    }
+    this.attach(insertAtStart);
+  }
+  private attach(insertAtStart: boolean) {
+    // this.element holds importedNode, which is the content of the template we want to insert. We are attaching all the template content of project list into hostElement, which is the pointer towards the app div that we will be rendering all the info. (basically <div id="root")
+      this.hostElement.insertAdjacentElement(insertAtStart ? 'afterbegin' : "beforeend", this.element); //insertedAdjacentElement(<where to render>, <what to render>) 
 }
 
 // ProjectList Class. Will render all projects onto a list
