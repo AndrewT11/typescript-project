af923c6c952b69bc178020c7592bbeb3ab868192
Andrew Tran <tranandrew@gmail.com>
2022-07-21 13:07:06 -0500

    added build to package.json scripts wepack.config.js add devltool, module, and resolve script src changed to 'dist/bundle.js' for webpack bundled code

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+        {
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 };
